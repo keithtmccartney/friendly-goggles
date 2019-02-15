f4371f42471407fea0195e40a2a8c435974e7445
Keith <keithtmccartney@hotmail.com>
2019-02-15 21:42:45 +0000

    Applying changes to 'user' service.

--- friendly-goggles/src/app/services/user.service.ts
+++ friendly-goggles/src/app/services/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+import { BaseService } from './base.service';
+import { User } from '../models/user';
+import { AppConfig } from '../config/config';
+import { Helpers } from '../helpers/helpers';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
+@Injectable()
 
-  constructor() { }
+export class UserService extends BaseService {
+  private apiURL = this.config.setting['API_URL'];
+
+  constructor(private http: HttpClient, private config: AppConfig, helper: Helpers) { super(helper); }
+
+  /* GET heroes from the server */
+
+  getUsers(): Observable<User[]> {
+    return this.http.get(this.apiURL + 'user', super.header()).pipe(
+      catchError(super.handleError));
+  }
 }
