8e3a2b72e62c74fce25ab89a1d7e92420bc2ca5d
Keith <keithtmccartney@hotmail.com>
2019-02-16 14:53:59 +0000

    Adding package changes to fix some issues ("npm install --save rxjs-compat").

--- friendly-goggles/src/app/app-routing.module.ts
+++ friendly-goggles/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './helpers/canActivateAuthGuard';
 import { LoginComponent } from './components/login/login.component';
-import { LogoutComponent } from './components/login/logout.component';
+import { LogoutComponent } from './components/logout/logout.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UsersComponent } from './components/users/users.component';
 
